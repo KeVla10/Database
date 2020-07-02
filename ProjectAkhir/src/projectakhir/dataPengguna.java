63d878593b5b19f76ccfe7b27f22ef2eb7a356af
KeVla10 <maulanakevin37@gmail.com>
2020-07-02 21:13:59 +0700

    Add files via upload

---
+++ ProjectAkhir/src/projectakhir/dataPengguna.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectakhir;
+
+/**
+ *
+ * @author Kevla
+ */
+public class dataPengguna {
+    private String username;
+    private String password;
+    private String nama_06911;
+    private String alamat;
+    private String no_tlp;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNama() {
+        return nama_06911;
+    }
+
+    public void setNama(String nama) {
+        this.nama_06911 = nama_06911;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getNo_tlp() {
+        return no_tlp;
+    }
+
+    public void setNo_tlp(String no_tlp) {
+        this.no_tlp = no_tlp;
+    }
+    
+        
+}
